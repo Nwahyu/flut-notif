ff6fcd88271b571cf5606e8783ecdf2535fefe09
affandynaufal6 <affandynaufal6@gmail.com>
2025-03-26 09:49:26 +0000

    otw

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import workmanager
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,13 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    WorkmanagerPlugin.setPluginRefistrantCallback {registry in GeneratedPluginRegistrant.register(with: registry)}
+
+    WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "task-identifier")
+    WorkmanagerPlugin.registerPeriodicTask(withIdentifier: "com.dicoding.notificationApp", requency: NSNumber(value: 16 * 16))
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
 }
